add6751154c7a5abc11295a94da9c9406e9623cc
Timothy Li <himynameistimli@gmail.com>
2018-07-09 20:04:40 +0800

    Wrap in UMD loader

--- build/ng-lodash.js
+++ build/ng-lodash.js
@@ -1,3 +1,22 @@
+// your library here
+(function (root, factory) {
+  'use strict';
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(['angular'], factory);
+  } else if (typeof module !== 'undefined' && typeof module.exports === 'object') {
+    // CommonJS support (for us webpack/browserify/ComponentJS folks)
+    module.exports = factory(require('angular'));
+  } else {
+    // in the case of no module loading system
+    // then don't worry about creating a global
+    // variable like you would in normal UMD.
+    // It's not really helpful... Just call your factory
+    return factory(root.angular);
+  }
+}(this, function (angular) {
+  'use strict';
+
 /**
  * @license
  * lodash 4.6.1 (Custom Build) <https://lodash.com/>
@@ -13598,3 +13617,7 @@ angular.module('ngLodash', []).constant('lodash', null).config([
     $provide.constant('lodash', _);
   }
 ]);
+
+
+  return 'ngLodash'; // the name of your module
+}));
